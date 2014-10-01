ff7d7b39a24b77dd76ab80aeaa82308629744fa2
jeremyhorgan <jeremyhorgan@hotmail.com>
2014-10-01 12:05:32 +0100

    Added Embedded sample adapted from Owin samples
    Embedded sample adapted from Owin samples to serve any file content

---
+++ Embedded/Startup.cs
@@ -0,0 +1,24 @@
+﻿using Owin;
+using Microsoft.Owin;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.Owin.StaticFiles;
+using Microsoft.Owin.FileSystems;
+
+namespace Embedded
+{
+    public class Startup
+    {
+        // Invoked once at startup to configure your application.
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseErrorPage();
+
+            FileServerOptions options = new FileServerOptions();
+            options.StaticFileOptions.ServeUnknownFileTypes = true;
+            options.EnableDirectoryBrowsing = true;
+
+            app.UseFileServer(options);
+        }
+    }
+}
